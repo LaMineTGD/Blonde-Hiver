ed68b22a849c37fee3d56bdedd90bbeb535e2a2a
Gnurf <gnurf.de.la.grande.cahute@gmail.com>
2023-07-22 00:09:48 +0200

    Bouton reload

--- Assets/Scripts/MacroManager.cs
+++ Assets/Scripts/MacroManager.cs
@@ -1,8 +1,29 @@
 using UnityEngine;
 using extOSC;
+using UnityEngine.SceneManagement;
 
 public class MacroManager : MonoBehaviour
 {
     public static MacroManager Instance { get; private set; }
+
     [HideInInspector] public float AbletonFactor = 0.00390625f;
+
+    public bool m_DebugMode;
+    public GameObject m_DebugUI;
+
+    void Start()
+    {
+        if(m_DebugMode)
+        {
+            m_DebugUI.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
 }
